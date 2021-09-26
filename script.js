7f7c4c863d4b3a842fc9138cdcee62911b127679
Durgesh Singh <55972645+durgesh789@users.noreply.github.com>
2021-09-26 10:38:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+const btn = document.getElementById("btn");
+const ul = document.getElementById("ul");
+const iTag = document.getElementsByTagName("i");
+const li = document.getElementsByTagName("li")
+
+
+// adding into todo list
+btn.addEventListener("click",() => {
+    var text = "";
+    text = prompt("enter your task");
+    if (text !== "") {
+        let liEle = document.createElement("li");
+        liEle.innerHTML = text;
+        let dustBin = document.createElement("i");
+        dustBin.className = "far fa-trash-alt";
+        liEle.appendChild(dustBin);
+        ul.appendChild(liEle);
+    } else {
+        alert("enter something");
+    }
+})
+
+
+// removing from to do list
+
+for (let i = 0; i < iTag.length; i++) {
+  
+    ul.onclick = e => {
+
+        if (e.target.className === "far fa-trash-alt"){
+        let x = e.target.parentNode;
+        x.style.display = "none";}
+    }
+};
+
+
+// task done
+
+for (let i = 0; i < li.length; i++) {
+
+    li[i].onclick = e => {
+        if (e.target.tagName === "LI") {
+            if (e.target.style.textDecoration === "line-through") {
+                e.target.style.textDecoration = "none";
+                e.target.style.backgroundColor = "#ADD2C2"
+            } else {
+                e.target.style.textDecoration = "line-through";
+                e.target.style.backgroundColor = "grey"
+            }
+
+            
+        }
+    }
+}
